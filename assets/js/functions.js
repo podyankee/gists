5f1f1ed4c5eebe64048e1e0d251c51da2139ac64
podyankee <podyankee@gmail.com>
2016-07-09 20:54:30 +0300

    Header finished

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,12 @@
+// .mobile-menu проблеьа рпи ресайзе
 
 
 $(document).ready(function(){
 
-
+  $(".toggle-mnu").click(function() {
+    $(this).toggleClass("on");
+    $(".mnu-mobile").slideToggle();
+    return false;
+  });
 
 });
