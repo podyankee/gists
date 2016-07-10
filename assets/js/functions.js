71e29597fa20070bcab0238e38f9a4f72b60ca9e
podyankee <podyankee@gmail.com>
2016-07-10 22:42:49 +0300

    Section profiles finished

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,14 @@
-// .mobile-menu проблеьа рпи ресайзе
+// .mobile-menu проблема при ресайзе
+// .button-accent проблема после нажатия
 
 
 $(document).ready(function(){
+
+  $(".mouse-icon").click(function(){
+    $("html, body").animate({
+      scrollTop : $(".s-adv").offset().top
+    }, 800);
+  });
 
   $(".toggle-mnu").click(function() {
     $(this).toggleClass("on");
